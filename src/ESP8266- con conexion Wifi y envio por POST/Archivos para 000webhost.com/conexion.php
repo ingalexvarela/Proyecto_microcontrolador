2f801c0ff2bccba4b2c83ff57208e6d7c99a8989
Alex Varela Quirós <98862101+ingalexvarela@users.noreply.github.com>
2023-02-27 11:14:04 -0600

    Archivos php para configuración del servidor web

---
+++ src/ESP8266- con conexion Wifi y envio por POST/Archivos para 000webhost.com/conexion.php
@@ -0,0 +1,10 @@
+<?php
+ 
+        $user = "id20349084_proyectoiot";
+        $pass = "Montaña1212.";
+        $server = "localhost";
+        $db ="id20349084_bd_esp8266";
+        $con = mysqli_connect($server, $user, $pass, $db);
+
+?>
+
